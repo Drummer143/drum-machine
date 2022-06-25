cef063b1f58b2b216287ee823874db982c2c0e59
Drummer143 <berg.evg.1@gmail.com>
2022-06-25 21:57:53 +0300

    feat: made keypad

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
-import Drumpad from './../Drumpad/Drumpad';
+import Setup from './../Setup/Setup';
+import Keypad from '../Keypad/Keypad';
+import { setOne, setTwo } from './../../sampleSet';
 
 import './App.css';
 
 function App() {
     return (
         <div className="app">
-            <Drumpad />
+            <Keypad sampleSet={setOne}/>
+            {/* <Setup /> */}
         </div>
     );
 }
--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,7 +1,18 @@
-function Keypad() {
+import './Keypad.css';
+
+const Button = ({ keyParams }) => {
+    const { keyTrigger, url } = keyParams;
+    const sampleAudio = new Audio(url);
+
+    return <button type='button' onClick={() => sampleAudio.play()}>{keyTrigger}</button>;
+};
+
+function Keypad({ sampleSet }) {
+    const buttons = sampleSet.map((key, i) => <Button key={i} keyParams={key} />);
+
     return (
-        <div>
-            Keypad
+        <div className="keypad">
+            {buttons}
         </div>
     );
 }
